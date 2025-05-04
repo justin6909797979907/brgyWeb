f2830a02b008037bd7e4e136de1b8311ae09596f
justin6909797979907 <heredajustinq@gmail.com>
2025-05-05 01:53:30 +0800

    update styles, add new images, and enhance navigation functionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,52 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
+// Open the menu
 menuOpenButton.addEventListener("click", () => {
     document.body.classList.add("show-mobile-menu");
 });
 
+// Close the menu
 menuCloseButton.addEventListener("click", () => {
     document.body.classList.remove("show-mobile-menu");
+});
+
+// Close the menu when any nav link is clicked
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        document.body.classList.remove("show-mobile-menu");
+    });
+});
+
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grapCursor: true,
+    spaceBetween: 25,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
 });
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -22,14 +22,114 @@
                 <div class="hero-details">
                     <h2 class="title">Barangay Patubig</h2>
                     <h3 class="subtitle">Barangay na maasahan sa anumang oras</h3>
-                    <p class="description">Welcome in our barangay Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, blanditiis.</p>
+                    <p class="description">Tulong-tulong, sama-sama para sa maayos, malinis at tahimik na pamayanan.
+                    </p>
                     <div class="buttons">
                         <a href="" class="button come-now">Come now</a>
                         <a href="" class="button contact-us">Contact Us</a>
                     </div>
                 </div>
                 <div class="hero-image-wrapper">
-                    <img src={{asset('images/patubig-logo.jpg')}} alt="hero-image">
+                    <img src={{asset('images/patubig-logo.png')}} alt="hero-image">
+                </div>
+            </div>
+        </section>
+
+        {{-- about-section --}}
+        <section class="about-section" id="about">
+            <div class="section-content">
+                <div class="image-wrapper">
+                    <img src={{asset('images/')}} alt="About" class="about-image">
+                </div>
+                <div class="about-details">
+                    <h2 class="section-title">About Us</h2>
+                    <p class="text">Barangay Patubig is a peaceful, progressive, and highly urbanized community in Marilao, Bulacan. While it is home to numerous factories and industrial establishments that drive local economic activity, it remains a clean, green, and safe environment. The barangay consists of cooperative and morally upright residents, ambitious and responsive business owners and corporate managers, and committed, visionary leaders. Together, they work hand in hand to sustain a healthy community and promote the continuous physical and economic growth of Barangay Patubig.</p>
+                    <div class="social-link-list">
+                        <a href="#" class="social-link"><i class="fa-brands fa-facebook"></i></a>
+                        <a href="#" class="social-link"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#" class="social-link"><i class="fa-brands fa-x-twitter"></i></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        {{-- services-section --}}
+        <section class="service-section" id="services">
+            <h2 class="section-title">Services</h2>
+            <div class="section-content">
+                <ul class="service-list">
+                    <li class="service-item">
+                        <img src={{asset('images/patubig-logo.png')}} alt="img" class="service-image">
+                        <h3 class="name">Lorem, ipsum.</h3>
+                        <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quod!</p>
+                    </li>
+                    <li class="service-item">
+                        <img src={{asset('images/patubig-logo.png')}} alt="img" class="service-image">
+                        <h3 class="name">Lorem, ipsum.</h3>
+                        <p class="text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates, placeat.</p>
+                    </li>
+                    <li class="service-item">
+                        <img src={{asset('images/patubig-logo.png')}} alt="img" class="service-image">
+                        <h3 class="name">Lorem, ipsum.</h3>
+                        <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolore!</p>
+                    </li>
+                    <li class="service-item">
+                        <img src={{asset('images/patubig-logo.png')}} alt="img" class="service-image">
+                        <h3 class="name">Lorem, ipsum.</h3>
+                        <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, totam.</p>
+                    </li>
+                    <li class="service-item">
+                        <img src={{asset('images/patubig-logo.png')}} alt="img" class="service-image">
+                        <h3 class="name">Lorem, ipsum.</h3>
+                        <p class="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda, aperiam.</p>
+                    </li>
+                    <li class="service-item">
+                        <img src={{asset('images/patubig-logo.png')}} alt="img" class="service-image">
+                        <h3 class="name">Lorem, ipsum.</h3>
+                        <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, placeat!</p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+
+        {{-- annoncements-section --}}
+        <section class="announcement-section" id="announcement">
+            <h2 class="section-title">Announcements</h2>
+            <div class="section-content">
+                <div class="slider-container swiper">
+                    <div class="slider-wrapper">
+                        <ul class="announcement-list swiper-wrapper">
+                            <li class="announcement swiper-slide">
+                                <img src={{asset('images/patubig-logo.png')}} alt="img" class="announcement-image">
+                                <h3 class="name">Lorem, ipsum.</h3>
+                                <i class="feedback">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, quidem!</i>
+                            </li>
+                            <li class="announcement swiper-slide">
+                                <img src={{asset('images/patubig-logo.png')}} alt="img" class="announcement-image">
+                                <h3 class="name">Lorem, ipsum.</h3>
+                                <i class="feedback">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, quidem!</i>
+                            </li>
+                            <li class="announcement swiper-slide">
+                                <img src={{asset('images/patubig-logo.png')}} alt="img" class="announcement-image">
+                                <h3 class="name">Lorem, ipsum.</h3>
+                                <i class="feedback">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, quidem!</i>
+                            </li>
+                            <li class="announcement swiper-slide">
+                                <img src={{asset('images/patubig-logo.png')}} alt="img" class="announcement-image">
+                                <h3 class="name">Lorem, ipsum.</h3>
+                                <i class="feedback">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, quidem!</i>
+                            </li>
+                            <li class="announcement swiper-slide">
+                                <img src={{asset('images/patubig-logo.png')}} alt="img" class="announcement-image">
+                                <h3 class="name">Lorem, ipsum.</h3>
+                                <i class="feedback">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, quidem!</i>
+                            </li>
+                        </ul>
+
+                        <div class="swiper-pagination"></div>
+                        <div class="swiper-slide-button swiper-button-prev"></div>
+                        <div class="swiper-slide-button swiper-button-next"></div>
+                    </div>
                 </div>
             </div>
         </section>
