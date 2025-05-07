9dadcf40dd5a3511cd5f473161f9671be1ba5f8d
onomatopoeia69 <carljameslangres@gmail.com>
2025-05-07 10:03:31 +0800

    adding accurate location of brgy

--- public/js/location.js
+++ public/js/location.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const map = L.map('map').setView([14.7573, 120.9486], 15);
+    const map = L.map('map').setView([14.775663181415643, 120.95956319002052], 15);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
             '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker([14.7573, 120.9486])
+    L.marker([14.775663181415643, 120.95956319002052])
         .addTo(map)
         .bindPopup('Barangay Patubig')
         .openPopup();
